957e1f15b4bba3c5542df10a16c13551867bb61e
Luis <luis.zugasti@outlook.com>
2020-12-20 19:29:05 -0500

    Start day 7 part 2

--- day7/day7.py
+++ day7/day7.py
@@ -16,7 +16,7 @@ def traverse_bags(bags: Dict[str, Dict[str, int]], bags_to_search: Set[str], goa
             pass
 
 
-def process_rule(rule: str):  # -> Tuple[str, Dict[str, int]]
+def process_rule(rule: str) -> Tuple[str, Dict[str, int]]:
     """ for each rule, returns a (bag_color, internal_bags) representation."""
     # I couldn't find anything in Python that would be extremely complicated without using lookaheads it seems,
     # so I am employing a simple pipeline of splitting.
@@ -48,6 +48,32 @@ def process_all_rules(rules: List[str]) -> Tuple[Dict[str, Dict[str, int]], set]
     return rule_dict, unique_colors
 
 
+def find_num_internal_bags(rules: List[str], bag_color: str) -> int:
+    """ TODO: refactor"""
+    rules, unseen_colors = process_all_rules(rules)
+    current_colors = set()
+
+    for rule_bag in rules.keys():
+        if bag_color in rules[rule_bag].keys():
+            current_colors.add(rule_bag)
+            unseen_colors.remove(rule_bag)
+
+    while len(current_colors) > 0:
+        new_colors = set()
+        remove_colors = set()
+        for rule_bag in unseen_colors:
+            for follow_on_bag in current_colors:
+                if follow_on_bag in rules[rule_bag].keys():
+                    new_colors.add(rule_bag)
+                    remove_colors.add(rule_bag)
+
+        current_colors = new_colors
+        for color in remove_colors:
+            unseen_colors.remove(color)
+
+    return len(rules) - len(unseen_colors)
+
+
 def find_all_bag_colors(rules: List[str], bag_color: str) -> int:
     rules, unseen_colors = process_all_rules(rules)
     current_colors = set()
@@ -74,7 +100,7 @@ def find_all_bag_colors(rules: List[str], bag_color: str) -> int:
 
 
 if __name__ == '__main__':
-    puzzle_input = open_file("day7_input.txt")
-    answer = find_all_bag_colors(puzzle_input, "shiny gold")
+    puzzle_input = open_file("day7_sample_input.txt")
+    answer = find_num_internal_bags(puzzle_input, "shiny gold")
 
     print("answer: {}".format(answer))
